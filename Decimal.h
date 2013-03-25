a57ea4203ec5bff65161aa88a6b600647d29f686
Dylan Lloyd <dylan@dylansserver.com>
2013-03-25 17:33:59 -0300

    whoops, remove main from .h

--- Decimal.h
+++ Decimal.h
@@ -109,33 +109,3 @@ ostream& operator<<(ostream& out, Decimal& decimal) {
                << " . "
                << decimal.fraction;
 }
-
-int main(int argc, char *argv[]) {
-    puts("\n-2.5");
-    Decimal test= Decimal("-2.5");
-    cout << test << endl;
-    puts("\n12.375");
-    test = Decimal("12.375");
-    cout << test << endl;
-    puts("\n1.375");
-    test = Decimal("1.375");
-    cout << test << endl;
-    puts("\n1");
-    test = Decimal("1");
-    cout << test << endl;
-    puts("\n.25");
-    test = Decimal("0.25");
-    cout << test << endl;
-    puts("\n.375");
-    test = Decimal("0.375");
-    cout << test << endl;
-
-    Decimal a = Decimal("1");
-    cout << a << endl;
-    Decimal b = Decimal("2");
-    cout << b << endl;
-    Decimal c = Decimal("1");
-    cout << c << endl;
-    if (a == b) puts("\nuh-oh");
-    if (a == c) puts("\nok");
-}
